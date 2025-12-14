381cd6dd6be12c7bbf9314d665139bb3d6e13c9a
Logika <logikavarash@gmail.com>
2025-12-14 15:31:16 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -3,18 +3,53 @@ let products=[
         id:1,
         name:"Активоване вугілля",
         image:"images/1189907e-97d8-4b80-8d84-aef70cd4c70f.webp",
-        type:"digestive",
+        type:"antiviral",
         price:100
     },
     {
         id:2,
         name:"Новирін",
         image:"images/img_0.webp",
-        type:"temp",
+        type:"antiviral",
         price:500
+    },
+    {
+        id:3,
+        name:"Парацетамол",
+        image:"images/news_163.jpg",
+        type:"analgesic",
+        price:101
+    },
+    {
+        id:4,
+        name:"Золофт",
+        image:"images/img_0 (1).webp",
+        type:"antidepressant",
+        price:660
+    },
+    {
+        id:5,
+        name:"Адаптол",
+        image:"images/4750258314515.webp",
+        type:"antidepressant",
+        price:400
+    },
+    {
+        id:6,
+        name:"Фітофорте Пробіотик",
+        image:"images/shopping.webp",
+        type:"antibiotic",
+        price:350
+    },
+    {
+        id:7,
+        name:"Лірика",
+        image:"images/26059_lOIaWLCM_20241126_121650.jpg",
+        type:"analgesic",
+        price:360
     }
 ];
-let cart=[];
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
 let productsContainer= document.querySelector(".cards-div");
 let btnGroup= document.querySelector(".dropdown-menu")
 
@@ -31,9 +66,9 @@ function renderProducts(items) {
                 <div class="card-img"><img src="${item.image}"></div>
                 <div class="card-info">
                     <div class="card-name">${item.name}</div>
-                    <div class="card-cost">${item.price}</div>
+                    <div class="card-cost">${item.price} грн </div>
                     <div class="button-to-s-card">
-                        <button type="button" class="btn btn-primary"> </button>
+                        <button type="button" class="btn btn-primary add-to-cart-btn"> </button>
                     </div>
                 </div>
             </artcle>
@@ -52,17 +87,28 @@ function applyFilters(categoryType){
     }
 }
 function addToCart(productId){
-    let product= products.find(p=>p.id==productId)
-    if (product){
-        cart.push(product);
-        alert("Товар додано " + product.name)
+    let cartProduct= cart.find(p=>p.id==productId);
+    if(cartProduct){
+        cartProduct.quantity+=1;
     }
+    else{
+        let product=products.find(p=>p.id==productId);
+        cart.push({...product,quantity:1});
+    }
+    // if (product){
+    //     cart.push(product);
+    //     alert("Товар додано " + product.name)
+    // }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert("Товар додано!")
 
 }
 let productsMap={
-    "Digestive": "digestive",
-    "For Nose": "fornose",
-    "For a temperature": "temp",
+    "Analgesics": "analgesic",
+    "Antibiotics": "antibiotic",
+    "Antihypertensives": "antihypertensive",
+    "Antidepressants":"antidepressant",
+    "Antivirals":"antiviral",
     "All": "all"
 }
 function setupFilterButtons(){
@@ -76,7 +122,7 @@ function setupFilterButtons(){
     }
 }
 productsContainer.addEventListener("click", function(event){
-    if(event.target.classList.contains(".button-to-s-card")){
+    if(event.target.classList.contains("add-to-cart-btn")){
         let productCard=event.target.closest(".card");
         let productId=parseInt(productCard.dataset.id)
         addToCart(productId)
